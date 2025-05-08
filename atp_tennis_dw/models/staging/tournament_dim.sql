49e4d137250d4b7be40d48549d45194b52254525
nahumsa <nahumsaa@gmail.com>
2025-05-07 23:36:42 -0300

    feat: add the tournament dimension

--- atp_tennis_dw/models/staging/tournament_dim.sql
+++ atp_tennis_dw/models/staging/tournament_dim.sql
@@ -9,7 +9,6 @@ SELECT
   tourney_name AS desc_tournament_name,
   surface AS desc_surface,
   best_of AS desc_best_of,
-  STRPTIME(CAST(tourney_date AS STRING), '%Y%m%d') AS dt_tournament_start_date,
-  updated_at::timestamp AS updated_at
+  STRPTIME(CAST(tourney_date AS STRING), '%Y%m%d') AS dt_tournament_start_date
 FROM matches
 WHERE UPPER(tourney_name) NOT LIKE '%DAVIS CUP%'
