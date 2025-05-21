6a142f51171e34a3eb95a28ccc1e569e75e36e42
nahumsa <nahumsaa@gmail.com>
2025-05-21 00:34:09 -0300

    feat: add indexing to players_dim

--- atp_tennis_dw/models/marts/mart_players_dim.sql
+++ atp_tennis_dw/models/marts/mart_players_dim.sql
@@ -1,8 +1,20 @@
+{{ config(
+    materialized = 'table',
+    indexes=[
+      {'columns': ['sk_player_key'], 'type': 'hash'},
+    ]
+)}}
 WITH players_dim AS (
 	SELECT * FROM {{ ref('players_dim') }}
 )
 
 SELECT
-  *
+  sk_player_key,
+  nk_id,
+  desc_name,
+  desc_hand,
+  mtr_year_of_birth,
+  mtr_height,
+  desc_ioc
 FROM
     players_dim
