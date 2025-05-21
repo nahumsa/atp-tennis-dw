3eda9cf8d3391711678443f175ee4aabcc6404e1
nahumsa <nahumsaa@gmail.com>
2025-05-21 00:03:51 -0300

    feat: add head_to_head table

---
+++ atp_tennis_dw/models/marts/head_to_head.sql
@@ -0,0 +1,26 @@
+WITH matches_fact AS (
+    SELECT * FROM {{ ref('matches_fact') }}
+), players_dim AS (
+	SELECT * FROM {{ ref('players_dim') }}
+)
+
+SELECT
+    CASE WHEN w.sk_player_key < l.sk_player_key THEN w.desc_name ELSE l.desc_name END AS player1,
+    CASE WHEN w.sk_player_key < l.sk_player_key THEN l.desc_name ELSE w.desc_name END AS player2,
+    COUNT(*) AS matches,
+    SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN w.sk_player_key ELSE l.sk_player_key END THEN 1 ELSE 0 END) AS p1_wins,
+    SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN l.sk_player_key ELSE w.sk_player_key END THEN 1 ELSE 0 END) AS p2_wins,
+    ROUND(AVG(mf.mtr_minutes), 2) AS average_duration_time,
+    SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN w.sk_player_key ELSE l.sk_player_key END THEN mf.nst_winner_stats.ace ELSE mf.nst_loser_stats.ace END) AS p1_total_aces,
+    SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN l.sk_player_key ELSE w.sk_player_key END THEN mf.nst_winner_stats.ace ELSE mf.nst_loser_stats.ace END) AS p1_total_aces,
+  SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN w.sk_player_key ELSE l.sk_player_key END THEN mf.nst_winner_stats.ace ELSE mf.nst_loser_stats.ace END) AS p1_total_aces,
+    SUM(CASE WHEN mf.fk_winner_id = CASE WHEN w.sk_player_key < l.sk_player_key THEN l.sk_player_key ELSE w.sk_player_key END THEN mf.nst_winner_stats.ace ELSE mf.nst_loser_stats.ace END) AS p2_total_aces,
+FROM
+    matches_fact mf
+JOIN
+    players_dim w ON mf.fk_winner_id = w.sk_player_key
+JOIN
+    players_dim l ON mf.fk_loser_id = l.sk_player_key
+GROUP BY
+    CASE WHEN w.sk_player_key < l.sk_player_key THEN w.desc_name ELSE l.desc_name END,
+    CASE WHEN w.sk_player_key < l.sk_player_key THEN l.desc_name ELSE w.desc_name END
