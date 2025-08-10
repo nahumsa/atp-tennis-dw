592afde6e87be916188d0ef9e79aea6789fe47b6
nahumsa <nahumsaa@gmail.com>
2025-08-10 20:02:35 -0300

    feat: add option to load to motherduck

--- load_data/atp_matches.py
+++ load_data/atp_matches.py
@@ -2,19 +2,25 @@ import dlt
 import requests
 import csv
 import time
+import argparse
 from datetime import datetime, timezone
 from typing import Iterator, Dict
 from io import StringIO
 
 
-@dlt.resource(primary_key=("tourney_id", "match_num"), name="atp_matches", write_disposition="replace")
+@dlt.resource(
+    primary_key=("tourney_id", "match_num"),
+    name="atp_matches",
+    write_disposition="replace",
+)
 def atp_matches_data() -> Iterator[Dict]:
     for year in range(2019, 2024 + 1):
         # Change this to a logger
         print(f"loading year={year}")
 
         response = requests.get(
-            f"https://raw.githubusercontent.com/JeffSackmann/tennis_atp/refs/heads/master/atp_matches_{year}.csv", allow_redirects=True
+            f"https://raw.githubusercontent.com/JeffSackmann/tennis_atp/refs/heads/master/atp_matches_{year}.csv",
+            allow_redirects=True,
         )
 
         # add sleep to avoid 429 Error
@@ -29,16 +35,29 @@ def atp_matches_data() -> Iterator[Dict]:
             yield dict(**record, **{"updated_at": updated_at, "year": year})
 
 
-
 @dlt.source
 def atp_matches_source():
     return atp_matches_data
 
 
+def cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Load Tennis Data",
+        description="loads tennis data to duckdb or motherduck",
+    )
+
+    parser.add_argument(
+        "--backend", choices=["duckdb", "motherduck"], default="duckdb"
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = cli()
     pipeline = dlt.pipeline(
         pipeline_name="atp_matches_pipeline",
-        destination="duckdb",
+        destination=args.backend,
         dataset_name="raw_data",
     )
 
